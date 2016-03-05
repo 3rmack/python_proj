ab171e3dc428555c6f5489e7e8df92a3e06939db
vyacheslav.tsypylov <3rmack@gmail.com>
2016-03-05 13:45:04 +0300

    finished unicode3 working as in task

---
+++ print_unicode3.py
@@ -0,0 +1,46 @@
+import sys
+import unicodedata
+
+
+words = []
+
+
+#def compare(l1, l2):
+#    for val in l1:
+#        if val in l2:
+#            return True
+#        return False
+
+
+def print_unicode_table(words):
+    print("decimal  hex  chr {0:^40}".format("name"))
+    print("------- ----- --- {0:-<40}".format(""))
+    code = ord(" ")
+    end = sys.maxunicode
+    while code < end:
+        c = chr(code)
+        name = unicodedata.name(c, "*** unknown ***")  # получаем название символа в str
+        namelower = name.lower()  # конвертим все символы в нижний регистр
+        nameset = set(namelower.split())  # конвертим str в set
+        if words <= nameset:
+        #if compare(words, namelist):
+            #print(words)
+            #print(nameset)
+            print("{0:7} {0:5X} {0:^3c} {1}".format(code, name.title()))
+        code += 1
+
+
+if len(sys.argv) > 1:
+    if sys.argv[1] in ("-h", "--help"):
+        print("usage: {0} [string]".format(sys.argv[0]))
+        words = 0
+    else:
+        words = sys.argv[1:]  # получаем list аргументов
+        a = 0  # конвертим все элементы list в нижний регистр
+        while a < len(words):
+            words[a] = words[a].lower()
+            a += 1
+        words = set(words)  # конвертим в set
+if words != 0:
+    #print(words)
+    print_unicode_table(words)
