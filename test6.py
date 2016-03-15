e4be4431c07553ff28f0e482738070029cac210d
vyacheslav.tsypylov <3rmack@gmail.com>
2016-03-15 17:51:20 +0300

    task4 completed

--- test6.py
+++ test6.py
@@ -15,7 +15,27 @@
 # aircraft = Aircraft("Airbus", "A320-200", Seating(100, 220))
 # print(aircraft.manufacturer)
 
-L = [3, 56, 7, 11, 6]
-def func (a, b, c, d, e):
-    return(a*b*c*d*e)
-print(func(*L))
+# L = [3, 56, 7, 11, 6]
+# def func (a, b, c, d, e):
+#     return(a*b*c*d*e)
+# print(func(*L))
+
+
+leaps = [y for y in range(1900, 1940)]
+print(leaps)
+leaps = [y for y in range(1900, 1940) if y % 4 == 0]
+print(leaps)
+leaps = [y for y in range(1900, 1940) if (y % 4 == 0 and y % 100 != 0) or (y % 400 ==0)]
+print(leaps)
+
+codes = []
+for sex in "MF":  # мужская (Male), женская (Female)
+    for size in "SMLX":  # маленький, средний, большой, очень большой
+        if sex == "F" and size == "X":
+            continue
+        for color in "BGW":  # черный (Black), серый (Gray), белый (White)
+            codes.append(sex + size + color)
+print(codes)
+
+codes = [s + z + с for s in "MF" for z in "SMLX" for с in "BGW" if not (s == "F" and z == "X")]
+print(codes)
