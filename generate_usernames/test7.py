c34474528b093250e5c71ffe604d9b60412d0c0a
vyacheslav.tsypylov <3rmack@gmail.com>
2016-03-22 15:58:16 +0300

    statistics

--- generate_usernames/test7.py
+++
@@ -1,8 +0,0 @@
-import collections
-
-
-username = "test_iser"
-fields = ["test_forename", ]
-
-User = collections.namedtuple("User", "USERNAME FORENAME MIDDLENAME SURNAME ID")
-user = User(username, fields[FORENAME], fields[MIDDLENAME], fields[SURNAME], fields[ID])
