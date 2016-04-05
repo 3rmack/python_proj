2d48a45f1069aa5e41bfbfdb25c24178f29d0731
3rmack <3rmack@gmail.com>
2016-04-05 23:47:44 +0300

    chapter4 task

--- Chapter4/task/proc_list.py
+++ Chapter4/task/proc_list.py
@@ -8,19 +8,20 @@ path = "."
 class ExitError(Exception): pass
 
 
+class BackError(Exception): pass
+
+
 def main():
-    file_list = get_file_list()
+    file_list_temp = os.listdir(path)  # получаем список файлов во временный список
+    file_list = list(file_name for file_name in file_list_temp if file_name.endswith(".lst"))  # используем генератор списков для создания списка файлов, оканчивающихся на ".lst"
     print_output(file_list)
     while True:
-        user_input = get_user_input(1, file_list)
-        process_user_input(user_input, file_list)
-
-
-def get_file_list():
-    file_list_temp = os.listdir(path)  # получаем список файлов во временный список
-    file_list = list(file_name for file_name in file_list_temp if file_name.endswith(".lst"))  # используем
-    # генератор списков для создания списка файлов, оканчивающихся на ".lst"
-    return file_list
+        user_input = get_user_input(1)
+        try:
+            user_input = int(user_input)
+            process_user_input_int(user_input, file_list)
+        except ValueError:
+            process_user_input_str(user_input, file_list)
 
 
 def print_output(file_list):
@@ -28,18 +29,22 @@ def print_output(file_list):
         print("{0}: {1}".format(i+1, file_list[i]))  # выводим названия файлов как по ТЗ
 
 
-def get_user_input(input_type, file_list):
+def get_user_input(input_type):
     if input_type == 1:
         user_input = input("Choose filename: ")
-        # process_user_input(user_input, file_list)
     if input_type == 2:
         user_input = input("[A]dd [Q]uit: ")
+        avaliable_input = "AaQq"
+        if user_input in avaliable_input:
+            return user_input
+        else:
+            print("Invalid choise, select one of '{0}'".format(avaliable_input))
     if input_type == 3:
         user_input = input("[A]dd [D]elete [S]ave [Q]uit: ")
     return user_input
 
 
-def process_user_input(user_input, file_list):
+def process_user_input_int(user_input, file_list):
     try:
         if int(user_input) == 0:
             raise ExitError
@@ -47,15 +52,37 @@ def process_user_input(user_input, file_list):
             filo = file_list[int(user_input)-1]
             print_file(filo)
         else:
-            print("-- no items are in the list --")
-    except ValueError:
+            raise BackError
+    except ExitError:
+        sys.exit("Exit")
+    except BackError:
+        print("-- no items are in the list --")
+        return
+
+
+def process_user_input_str(user_input, file_list):
+    try:
         if user_input in file_list:
             print_file(user_input)
         else:
-            print("-- no items are in the list --")
+            user_input_2 = get_user_input(2)
+            if user_input_2 in "Aa":
+                if not user_input.endswith(".lst"):
+                    user_input = user_input+".lst"
+                print(user_input)
+                new_file = open(user_input, "w")
+                new_file.write("")
+                new_file.close()
+                file_list.append(user_input)
+                print_output(file_list)
+            else:
+                print_output(file_list)
+                return
     except ExitError:
-        print("Exiting...")
-        sys.exit()
+        sys.exit("Exit")
+    except BackError:
+        print("-- no items are in the list --")
+        return
 
 
 def print_file(user_input):
