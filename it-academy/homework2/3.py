23d2854661d6bf37c8f4aca6b9b62398956e1571
vyacheslav.tsypylov <3rmack@gmail.com>
2016-05-25 09:44:30 +0300

    it-academy homework2

--- it-academy/homework2/3.py
+++ it-academy/homework2/3.py
@@ -1,5 +1,7 @@
+# coding = utf-8
 my_dict = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
-dict_keys = list(my_dict.keys())  # делаем копию списка ключей, иначе они будут динамически менятся в цикле
-for key in dict_keys:
+# dict_keys = list(my_dict.keys())  # делаем копию списка ключей, иначе они будут динамически менятся в цикле
+# for key in dict_keys:
+for key in my_dict.keys():
     my_dict[key+str(len(key))] = my_dict.pop(key)
 print(my_dict)
